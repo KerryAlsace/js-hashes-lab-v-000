91c9b08f0c47efbfed784b8b29da0486ccda99a8
KerryAlsace <kerrydougherty@gmail.com>
2016-10-03 16:02:40 -0400

    Done.

--- recipe.js
+++ recipe.js
@@ -2,21 +2,21 @@
 
 function addIngredient(recipe, ingredient, amounts) {
   recipe[ingredient] = amounts;
-  return recipe
+  return recipe;
 }
 
 function removeIngredient(recipe, ingredient) {
   delete recipe[ingredient];
-  return recipe
+  return recipe;
 }
 
 function updateIngredient(recipe, ingredient, amount) {
-  recipe[ingredient] = amount
-  return recipe
+  recipe[ingredient] = amount;
+  return recipe;
 }
 
 function readRecipe(recipe) {
   for (var key in recipe) {
-    console.log(`this recipe calls for ${recipe[key]} of ${key}`)
+    console.log(`this recipe calls for ${recipe[key]} of ${key}`);
   }
 }
